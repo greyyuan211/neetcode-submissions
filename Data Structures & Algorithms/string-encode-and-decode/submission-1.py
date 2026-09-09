2d3298a525290571815dcd1530caa03868e524ef
Linwei Yuan <56415387+greyyuan211@users.noreply.github.com>
2026-09-08 22:12:12 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = [] # faster than string += operation
+        for s in strs:
+            res.append(str(len(s)))
+            res.append("?")
+            res.append(s)
+        #print(''.join(res))
+        return ''.join(res)
+
+    def decode(self, s: str) -> List[str]:
+        i = 0
+        res = []
+        # print(s[i])
+        while (i<len(s)):
+            j = i
+            while (s[j] != "?"):
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+            
+
+        return res
+
